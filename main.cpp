0a07bb63b0cd883f44cf7ec48225c485f51bf8e9
vasilykuzmin <42723244+vasilykuzmin@users.noreply.github.com>
2018-08-31 15:21:55 +0300

    Redone class World

--- main.cpp
+++ main.cpp
@@ -2,20 +2,43 @@
 
 using namespace std;
 
+struct cell
+{
+	int biome, structure, squad;
+};
+
 class World
 {
 	int size;
+	cell field[9][13];
 	
-public:
+	public:
 	World(int sz) : size(sz)
-	{}
+	{
+		for (int i = 0; i < 9; ++i)
+			for (int j = 0; j < 13; ++j)
+			{
+				//biome >> field[i][j].biome;
+				//structure >> field[i][j].structure;
+
+				field[i][j].biome = 0;
+				field[i][j].structure = 0;
+
+				field[i][j].squad = 0;
+			}
+	}
 
 	friend ostream& operator << (ostream& lhs, const World& rhs);
 };
 
 ostream& operator << (ostream& lhs, const World& rhs)
 {
-	lhs << rhs.size;
+	for (int i = 0; i < 9; ++i)
+	{
+		for (int j = 0; j < 13; ++j)
+			cout << rhs.field[i][j].squad << ' ';
+		cout << endl;
+	}
 	return lhs;
 }
 
